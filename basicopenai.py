7c7007b0289753467557b2d64c17003f1a0f9860
Nikole <nikole.mrs06@gmail.com>
2024-10-21 12:39:16 -0600

    azure

---
+++ basicopenai.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+secret_key="secret key"
+prompt = "Give me a summary"
+
+client = OpenAI (
+    api_key=secret_key,
+)
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo-0125",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Hello!"}
+  ],
+    max_tokens=1000,
+    temperature=0
+)
+
+print(completion)
